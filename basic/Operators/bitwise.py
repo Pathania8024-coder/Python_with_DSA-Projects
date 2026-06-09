6db47e53d898ed82d0a47f45ceca387717cd9463
Pathania8024-coder <codelikepathania@gmail.com>
2026-06-09 10:14:26 +0530

    Create Bitwise.py

---
+++ basic/Operators/bitwise.py
@@ -0,0 +1,14 @@
+a = 5
+b = 2
+c = a & b  # Bitwise AND
+d = a | b  # Bitwise OR
+e = a ^ b  # Bitwise XOR
+f = ~a     # Bitwise NOT
+g = a << 4 # Left shift
+h = a >> 2 # Right shift
+print("Bitwise AND:", c)  # Output: 1
+print("Bitwise OR:", d)   # Output: 7    
+print("Bitwise XOR:", e)  # Output: 6
+print("Bitwise NOT:", f)  # Output: -6
+print("Left Shift:", g)   # Output: 80
+print("Right Shift:", h)  # Output: 1
